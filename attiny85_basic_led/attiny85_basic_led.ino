7b43c5c766c492882ae360b005f017a792c6c01e
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-01 02:16:10 -0500

    Breathing LED cycle

--- attiny85_basic_led/attiny85_basic_led.ino
+++ attiny85_basic_led/attiny85_basic_led.ino
@@ -11,36 +11,44 @@ Nov 30, 2023
 #include "DigiKeyboard.h"
 #include <Wire.h>
 
-#define LED_PIN 1 // LED on Model A  4
+//#define LED_PIN 1 // LED on Model A  4
 //#define LED_PIN 0 // LED on Model B 
-#define SENSOR_PIN 5
+//#define SENSOR_PIN 4
 
-const byte I2C_SLAVE_ADDR = 0x27;
-
-int sensor_value = 0;
+unsigned int sensor_value = 0;
+unsigned int LED_PIN = 1;
+unsigned int SENSOR_PIN = 4;
 
 void setup() {             
   // Initialize the digital pin as an output
   pinMode(LED_PIN, OUTPUT);
-  Wire.begin();
+  pinMode(SENSOR_PIN, INPUT);
+  //Wire.begin();
 }
 
 void loop() {
 
   sensor_value = analogRead(SENSOR_PIN);
-  DigiKeyboard.print("ATtiny85. Sensor: ");
-  DigiKeyboard.println(sensor_value);
-  /*Wire.beginTransmission(SENSOR_PIN);
-  Wire.write(sensor_value);
-  Wire.endTransmission();*/
+  /*DigiKeyboard.print("ATtiny85. Sensor: ");
+  DigiKeyboard.println(sensor_value);*/
 
-  delay(1500); 
-  digitalWrite(LED_PIN, HIGH);
-  delay(250);              
-  digitalWrite(LED_PIN, LOW); 
-  delay(250);               
-  digitalWrite(LED_PIN, HIGH);
-  delay(250);                
-  digitalWrite(LED_PIN, LOW); 
-  delay(1500);
+  //delay(1500);
+  /*for (int n = 0; n < 3; n++)
+  {
+    digitalWrite(LED_PIN, HIGH);
+    delay(125);        
+    digitalWrite(LED_PIN, LOW); 
+    delay(125);
+  }*/
+  for (int n = 0; n < 255; n+=5)
+  {
+    analogWrite(LED_PIN, n);
+    delay(25);
+  } 
+  for (int n = 255; n >= 0; n--)
+  {
+    analogWrite(LED_PIN, n);
+    delay(25);
+  }           
+  //delay(1500);
 }
