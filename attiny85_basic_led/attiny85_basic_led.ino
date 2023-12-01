58cfc3cbcbf5b32339ce9255fefc5dc0d86e7f55
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-01 01:29:19 -0500

    .

--- attiny85_basic_led/attiny85_basic_led.ino
+++ attiny85_basic_led/attiny85_basic_led.ino
@@ -8,18 +8,21 @@ Nov 30, 2023
 
 */
 
-#include "DigiKeyboard.h" 
+#include "DigiKeyboard.h"
+#include <Wire.h>
 
-#define LED_PIN 1 // LED on Model A  
+#define LED_PIN 1 // LED on Model A  4
 //#define LED_PIN 0 // LED on Model B 
-#define SENSOR_PIN 3
-#define GPIO_ADDR 0x27
+#define SENSOR_PIN 5
+
+const byte I2C_SLAVE_ADDR = 0x27;
 
 int sensor_value = 0;
 
 void setup() {             
   // Initialize the digital pin as an output
-  pinMode(LED_PIN, OUTPUT); 
+  pinMode(LED_PIN, OUTPUT);
+  Wire.begin();
 }
 
 void loop() {
@@ -27,17 +30,17 @@ void loop() {
   sensor_value = analogRead(SENSOR_PIN);
   DigiKeyboard.print("ATtiny85. Sensor: ");
   DigiKeyboard.println(sensor_value);
+  /*Wire.beginTransmission(SENSOR_PIN);
+  Wire.write(sensor_value);
+  Wire.endTransmission();*/
 
-  //digitalWrite(0, HIGH);   
+  delay(1500); 
   digitalWrite(LED_PIN, HIGH);
-  delay(250);             
-  //digitalWrite(0, LOW);    
+  delay(250);              
   digitalWrite(LED_PIN, LOW); 
-  delay(250);             
-  //digitalWrite(0, HIGH);   
+  delay(250);               
   digitalWrite(LED_PIN, HIGH);
-  delay(250);             
-  //digitalWrite(0, LOW);    
+  delay(250);                
   digitalWrite(LED_PIN, LOW); 
   delay(1500);
 }
