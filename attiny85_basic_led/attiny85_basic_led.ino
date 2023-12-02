6b4cf872614a090b3f59bbfe5cd615273a4bc976
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-02 02:58:56 -0500

    I2C

--- attiny85_basic_led/attiny85_basic_led.ino
+++ attiny85_basic_led/attiny85_basic_led.ino
@@ -11,26 +11,43 @@ Nov 30, 2023
 #include "DigiKeyboard.h"
 #include <Wire.h>
 
-//#define LED_PIN 1 // LED on Model A  4
-//#define LED_PIN 0 // LED on Model B 
-//#define SENSOR_PIN 4
+//#define USB_OUTPUT
+//#define SERIAL_OUTPUT
 
 unsigned int sensor_value = 0;
+uint8_t sensor_raw = 0;
 unsigned int LED_PIN = 1;
-unsigned int SENSOR_PIN = A2; //4;
+int SENSOR_PIN = A2; //4;
 
 void setup() {             
   // Initialize the digital pin as an output
   pinMode(LED_PIN, OUTPUT);
   pinMode(SENSOR_PIN, INPUT_PULLUP);
-  //Wire.begin();
+  Wire.begin();
+  #ifdef SERIAL_OUTPUT
+  Serial.begin(9600);
+  Serial.println("ATTinu 85");
+  #endif
 }
 
 void loop() {
 
   sensor_value = analogRead(SENSOR_PIN);
-  DigiKeyboard.print("ATtiny85. Sensor: ");
+  //sensor = analogRead(SENSOR_PIN);
+  sensor_raw = analogRead(SENSOR_PIN);
+  #ifdef USB_OUTPUT
+  DigiKeyboard.print("ATtiny85. Sensor (");
+  DigiKeyboard.print(SENSOR_PIN);
+  DigiKeyboard.print("): ");
   DigiKeyboard.println(sensor_value);
+  #endif
+  #ifdef SERIAL_OUTPUT
+  Serial.print("ATtiny85. Sensor (");
+  Serial.print(SENSOR_PIN);
+  Serial.print("): ");
+  Serial.println(sensor_value);
+  #endif
+
 
   //delay(1500);
   /*for (int n = 0; n < 3; n++)
@@ -50,5 +67,8 @@ void loop() {
     analogWrite(LED_PIN, n);
     delay(125);
   }           
-  //delay(1500);
+
+  Wire.beginTransmission(10);
+  Wire.write(sensor_raw);
+  //Wire.endTransmission();
 }
