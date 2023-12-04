aafc99fb5284430b76adac23dc13320b6facaebf
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-04 04:02:08 -0500

    .

--- ATTiny85_I2C/src/main.cpp
+++ ATTiny85_I2C/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUILTIN_LED 2
+
+byte i2c_data, address, err;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin();
+  pinMode(BUILTIN_LED, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("I2C");
+
+  Serial.println("Starting I2C transmission ...");
+
+  for (address = 1; address < 127; address++)
+  {
+    Wire.beginTransmission(address);
+    if (Wire.endTransmission() == 0) {
+      Serial.print("I2C device was found at ");
+      Serial.println(address);
+      i2c_data = Wire.read();
+      Serial.print("Value: ");
+      Serial.println(i2c_data);
+    }
+    else if (Wire.endTransmission() == 4)
+      Serial.println("I2C unknown error.");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(250);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(250);
+
 }
