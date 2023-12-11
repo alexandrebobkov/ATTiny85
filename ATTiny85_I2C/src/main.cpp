171599e20d8c4a2d367b13015a5123454cdaf9ec
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-11 02:18:46 -0500

    comments

--- ATTiny85_I2C/src/main.cpp
+++ ATTiny85_I2C/src/main.cpp
@@ -2,8 +2,11 @@
   By: Alexander Bobkov
 
   December 4, 2023
+  Updated: December 11, 2023
 
-  Scans I2C bus and requests variable value from ATTiny85 device.
+  Scans I2C bus for devices and requests variable value from available ATTiny85 device(s).
+
+  Known issue: ATTiny85 stalls/stops sending values after 1-2 minutes.
   
 */
 #include <Arduino.h>
@@ -19,8 +22,8 @@ void setup() {
   Serial.begin(115200);
   Serial.println("I2C");
 
+  // Scan I2C bus
   Serial.println("I2C scan ...");
-
   for (address = 1; address < 127; address++)
   {
     Serial.print("Address: ");
@@ -33,26 +36,30 @@ void setup() {
       size_t buff_size = Wire.readBytes((uint8_t*)&i2c_data, 2);
       Serial.print("Value: ");
       Serial.println(i2c_data, HEX);
+      // Note address of available device.
       device_addr = address;
     }
     else if (Wire.endTransmission() == 4)
       Serial.println("I2C unknown error.");
     delay(500);
   }
-  //Wire.end();
 }
 
 void loop() {
 
+  // LED pulse blink.
   digitalWrite(BUILTIN_LED, HIGH);
   delay(250);
   digitalWrite(BUILTIN_LED, LOW);
   delay(250);
 
+  // Reset value
   i2c_data = 0;
+  // Request value from available device
   Serial.print("Listening to I2C device at address: ");
   Serial.println(device_addr, HEX);
   Wire.beginTransmission(device_addr);
+  // Display value if request succeeded
   if (Wire.endTransmission() == 0) {
     Wire.requestFrom(device_addr, 2);
     size_t buff_size = Wire.readBytes((uint8_t*)&i2c_data, 2);
@@ -60,5 +67,6 @@ void loop() {
     Serial.println(i2c_data, HEX);
     Serial.println();
   }
+  // Delay 1 second
   delay(1000);
 }
