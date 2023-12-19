0f3b812340ccb0efc6f59021cf3fbb70c418409b
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-18 23:09:23 -0500

    .

--- attiny85_lcd_1602/attiny85_lcd_1602.ino
+++ attiny85_lcd_1602/attiny85_lcd_1602.ino
@@ -1,4 +1,11 @@
-//#include "LiquidCrystal_I2C.h"
+/*
+  By: Alexander Bobkov
+  
+  Created: December 18, 2023
+
+  ATtiny85 displays text on LCD 1602 over I2C bus (ATtiny85 is set as Master).
+  
+*/
 #include "TinyWireM.h"
 #include "LiquidCrystal_attiny.h"
 
@@ -8,11 +15,11 @@ void setup() {
   lcd.init();
   lcd.clear();
   lcd.backlight();
-  //lcd.home();
-  //lcd.print("Hello Foxie");
+  //lcd.setContrast(80);
+  lcd.setCursor(0, 0);              // Line 1
   displayText("Hello Foxie");
-  //lcd.setCursor(0, 1);
-
+  lcd.setCursor(0, 1);              // Line 2
+  displayText("I love you");
 }
 
 void loop() {
