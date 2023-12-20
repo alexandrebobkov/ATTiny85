8fb20f282623908bcf1c2606a23c1b12540d63ad
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-20 03:32:59 -0500

    toggle LCD backlight

--- attiny85_lcd_1602/attiny85_lcd_1602.ino
+++ attiny85_lcd_1602/attiny85_lcd_1602.ino
@@ -6,8 +6,20 @@
   ATtiny85 displays text on LCD 1602 over I2C bus (ATtiny85 is set as Master).
   
 */
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #include "TinyWireM.h"
 #include "LiquidCrystal_attiny.h"
+
+#define INTERRUPT_PIN PCINT1  // This is PB1 per the schematic
+#define INT_PIN PB1           // Interrupt pin of choice: PB1 (same as PCINT1) - Pin 6
+#define LED_PIN PB4           // PB4 - Pin 3
+#define PCINT_VECTOR PCINT0_vect      // This step is not necessary - it's a naming thing for clarit
+
+uint8_t toggle = false;
+uint8_t state_prev = false;
+uint8_t state_curr = false;
+
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -23,6 +35,14 @@ void setup() {
   lcd.setCursor(0, 1);              // Line 2
   //displayText("I love you");
   displayText(" EIT  2023/2024");
+
+  // Setup Interrupts
+  pinMode(LED_PIN, OUTPUT);
+  cli();                            // Disable interrupts during setup
+  PCMSK |= (1 << INTERRUPT_PIN);    // Enable interrupt handler (ISR) for our chosen interrupt pin (PCINT1/PB1/pin 6)
+  GIMSK |= (1 << PCIE);             // Enable PCINT interrupt in the general interrupt mask
+  pinMode(INT_PIN, INPUT_PULLUP);   // Set our interrupt pin as input with a pullup to keep it stable
+  sei();  
 }
 
 void loop() {
@@ -35,5 +55,20 @@ void displayText(String text) {
   text.toCharArray(charBuf, 16);
   for (int i = 0; i < text.length(); i++) {
     lcd.print(charBuf[i]);
+  }
+}
+
+ISR(PCINT_VECTOR) {
+  state_curr = digitalRead(INT_PIN);
+  if (state_curr != state_prev) {
+    state_prev = state_curr;
+    if (!state_curr) {
+      toggle = (toggle) ? LOW : HIGH;
+      digitalWrite(LED_PIN, toggle);
+    }
+    if (toggle)
+      lcd.backlight();
+    else
+      lcd.noBacklight();
   }
 }
