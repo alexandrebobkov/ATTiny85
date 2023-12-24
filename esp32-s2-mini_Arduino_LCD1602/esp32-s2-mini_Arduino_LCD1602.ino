b8534b46703aee949e03a6595ee5c573b444dde3
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-24 13:07:11 -0500

    .

--- esp32-s2-mini_Arduino_LCD1602/esp32-s2-mini_Arduino_LCD1602.ino
+++ esp32-s2-mini_Arduino_LCD1602/esp32-s2-mini_Arduino_LCD1602.ino
@@ -1,32 +1,17 @@
-//#include <LCD-I2C.h>
-#include <LiquidCrystal_I2C.h>
-//#include "LiquidCrystal_attiny.h"
+#include <LCD-I2C.h>
 
-//LCD_I2C lcd(0x27, 16, 2);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-uint8_t snow[8] =
-{
-    0b01000,
-    0b11101,
-    0b01011,
-    0b00001,
-    0b00100,
-    0b01110,
-    0b00100,
-    0b10000
-};
+LCD_I2C lcd(0x27, 16, 2);
 
 void setup() {
   // put your setup code here, to run once:
+
   lcd.begin();
-  lcd.createChar(1, snow);
   lcd.clear();
   lcd.display();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print(" HAPPY HOLIDAYS ");
   lcd.write(0);
+  lcd.print(" HAPPY HOLIDAYS ");  
 }
 
 void loop() {
