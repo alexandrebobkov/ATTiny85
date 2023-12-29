3b83dc137739bd43df71b1e53c8edc5091b72c2b
Alexandre B <alexandre.bobkov@gmail.com>
2023-12-29 10:26:48 -0500

    .

--- esp32-s2-mini_IDF_I2C-slave/main/i2c_simple_main.c
+++ esp32-s2-mini_IDF_I2C-slave/main/i2c_simple_main.c
@@ -15,6 +15,9 @@
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
+
+   35 -> green
+   33 -> yellow
 */
 #include <stdio.h>
 #include "esp_log.h"
